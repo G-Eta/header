de2da0016101385c0aa7165921ae89de0a00859a
G-Eta <104272243+G-Eta@users.noreply.github.com>
2022-07-11 14:57:30 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 
 
+
+
 date=new Date();
 year = date.getFullYear();
 month = date.getMonth() + 1;
 day = date.getDate();
 document.getElementById("date-time").innerHTML = month + "/" + day + "/" + year;
     
+var day = date.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+console.log("Today is : " + daylist[day] + ".");
+document.getElementById("tDay").innerHTML = daylist[day];
+
